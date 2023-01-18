74672a66e0635d42d0387b397e395c4fa4aa4f53
ivan <ivanpetkovic@gmail.com>
2023-01-18 13:47:52 +0100

    polybar datetime, wallpaper support

--- .config/i3/bin/autostart.sh
+++ .config/i3/bin/autostart.sh
@@ -12,7 +12,7 @@ for _prs in "${_ps[@]}"; do
 	fi
 done
 
-# Setup monitors 
+# Setup monitors
 xrandr --output DVI-D-0 --off --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
 
 # Fix cursor
@@ -29,7 +29,8 @@ ksuperkey -e 'Super_L=Alt_L|F1' &
 ksuperkey -e 'Super_R=Alt_L|F1' &
 
 # Restore wallpaper
-hsetroot -root -cover ~/.config/i3/wallpapers/default.png
+# hsetroot -root -cover ~/.config/i3/wallpapers/default.png
+nitrogen --restore
 
 # Lauch notification daemon
 ~/.config/i3/bin/i3dunst.sh
